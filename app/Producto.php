c641cc0235fcc5c416d899f180c6e53da2186ed2
Rafael Olmino <volminos@miumg.edu.gt>
2020-11-07 03:47:58 -0600

    tercer commit

--- app/Producto.php
+++ app/Producto.php
@@ -8,4 +8,13 @@ class Producto extends Model
 {
     public $timestamps = false;
     protected $primaryKey = 'cod_pro';
+
+    protected $fillable = ['cod_pro',
+                                      'nom_pro',
+                                      'imagen',
+                                      'descripcion',
+                                      'precio_pro',
+                                      'stock',
+                                      'cod_cate',
+                                      'cod_pro'];
 }
